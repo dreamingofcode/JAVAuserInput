447be6ae3a7960138dcbfa57845586e351b34487
Cesar <cesarmota@ymail.com>
2020-10-08 09:08:25 -0500

    first commit

--- Sequencing.java
+++ Sequencing.java
@@ -1,7 +1,19 @@
-import java.until.Scanner;
-public class Sequencing {
-    public static void main(String[] args){
-Scanner keyborad= new Scanner(System.in);
+import java.util.Scanner;
 
+public class Sequencing {
+    public static void main( String[] args ){
+        Scanner keyboard= new Scanner(System.in);
+        double price  , salesTax, total;
+
+        // salesTax= price *0.0825;
+        
+        System.out.println("How much is the purchase price?");
+        price=  keyboard.nextDouble();
+        
+        System.out.println("What percentage is the sales tax?");
+        total= (keyboard.nextDouble() * price);
+        total= total + price;
+        // total= price + salesTax;
+        System.out.println("Your total: \t \t"+ total );
     }
 }
