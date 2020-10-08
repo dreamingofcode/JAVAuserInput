9c74aa8653c0ce6228250952e1a2adce5709aa42
Cesar <cesarmota@ymail.com>
2020-10-08 11:54:48 -0500

    word.compare()

--- BooleanExpressions.java
+++ BooleanExpressions.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BooleanExpressions {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean a, b, c, d, e, f, g;
+        boolean a, b, c, d, e, f;
         double x, y;
 
         System.out.println("Please provide me with a number: ");
---
+++ WordComparisonCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WordComparisonCheck {
+    public static void main(String[] args) {
+        Scanner input = new java.util.Scanner(System.in);
+        String word;
+        boolean yes, no;
+    
+
+        System.out.println("Type in the word \" PIZZA \" please");
+        word= input.next();
+
+        yes= word.equals("PIZZA");
+        no= ! word.equals("PIZZA");
+
+        System.out.println("You typed what was requested and you're an obidient person: "+yes);
+        System.out.println("You DID NOT type what was requested and you're AWFUL: "+no);
+
+    }
+}
