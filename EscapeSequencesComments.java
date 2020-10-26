6c5498bfaed55e5fcbac7233351518e85dcc08c5
Cesar <cesarmota@ymail.com>
2020-10-26 15:32:13 -0500

    update

--- EscapeSequencesComments.java
+++ EscapeSequencesComments.java
@@ -1,17 +1,16 @@
 public class EscapeSequencesComments{
   public static void main( String[] args){
- 4         System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
- 5         System.out.print( "\tLearn Java the \"Hard\" Way!\n" );
- 6         // System.out.frimp( "Learn Java the Hard Way" );
- 7         System.out.print( "Hello\n" ); // This line prints Hello.
- 8         System.out.print( "Jello\by\n" ); // This line prints Jelly.
- 9         /* The quick brown fox jumped over a lazy dog.
-10            Quick wafting zephyrs vex bold Jim. */
-11         System.out /* testing */ .println( "Hard to believe, eh?" );
-12         System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
-13         System.out.println( "\\ // -=- \\ //" );
-14         System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); // it takes 2 to make 1
-15         System.out.print( "I hope you understand \"escape sequences\" now.\n" );
-16         // and comments. :)
-17     }
-18 }
+          System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
+          System.out.print( "\tLearn Java the \"Hard\" Way!\n" );
+          // System.out.frimp( "Learn Java the Hard Way" );
+          System.out.print( "Hello\n" ); // This line prints Hello.
+          System.out.print( "Jello\by\n" ); // This line prints Jelly.
+          /* The quick brown fox jumped over a lazy dog.
+            Quick wafting zephyrs vex bold Jim. */
+        System.out /* testing */ .println( "Hard to believe, eh?" );
+         System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
+         System.out.println( "\\ // -=- \\ //" );
+         System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); // it takes 2 to make          System.out.print( "I hope you understand \"escape sequences\" now.\n" );
+         // and comments. :)
+     }
+ }
